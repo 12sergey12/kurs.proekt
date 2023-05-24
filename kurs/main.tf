bd75f64f60b8a8fcf98217c15c7e1fa0922c2f06
12sergey12 <122733494+12sergey12@users.noreply.github.com>
2023-05-24 22:29:19 +0700

    Update main.tf

--- kurs/main.tf
+++ kurs/main.tf
@@ -1,1 +1,17 @@
-t
+terraform {
+  required_version = "= 1.4.5"
+
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "= 0.73"
+    }
+  }
+}
+provider "yandex" {
+  token     = "y0_AgAAAABQoySwAATuwQAAAADh7F8nSIVDfa-GRkim2g4tsW6eqhGWnio"
+  cloud_id  = "b1gupe0u1qqfpl0fuip5"
+  folder_id = "b1gi8mor51pqsp67s6t9"
+  zone      = "ru-central1-a"
+}
+
